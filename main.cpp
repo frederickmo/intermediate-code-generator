7990f97963d047aae117bf363ddd846b086c7ec0
frederickmo <frederick_0428@tongji.edu.cn>
2022-01-04 18:35:30 +0800

    update

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@ int main() {
     QuadrupleTranslator::lex();
     QuadrupleTranslator::parse();
 
+    cout << "四元式形式\n";
     for (const auto& quadruple : newQuadrupleList) {
         quadruple.printAsQuadruple();
     }
     cout << endl;
 
+    cout << "三地址代码形式\n";
     for (const auto& quadruple : newQuadrupleList) {
         quadruple.printArgInName();
     }
